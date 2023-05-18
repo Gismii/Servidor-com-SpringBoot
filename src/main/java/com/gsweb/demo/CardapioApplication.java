d98b03f59515cdfd77bc8d3f2cff70605d13ae91
Gismii <gisme.guimaraes@gmail.com>
2023-05-18 20:21:23 -0300

    Cria busca por id

--- src/main/java/com/gsweb/demo/CardapioApplication.java
+++ src/main/java/com/gsweb/demo/CardapioApplication.java
@@ -1,13 +1,21 @@
 package com.gsweb.demo;
 
+
+import com.gsweb.demo.food.Food;
+import com.gsweb.demo.food.FoodResponseDTO;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class CardapioApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CardapioApplication.class, args);
+
+
+		System.out.println("Tudo ok");
 	}
+
 
 }
--- src/main/java/com/gsweb/demo/controller/FoodController.java
+++ src/main/java/com/gsweb/demo/controller/FoodController.java
@@ -4,11 +4,10 @@ import com.gsweb.demo.food.Food;
 import com.gsweb.demo.food.FoodRepository;
 import com.gsweb.demo.food.FoodRequestDTO;
 import com.gsweb.demo.food.FoodResponseDTO;
-import jakarta.persistence.Id;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("food")
@@ -19,20 +18,37 @@ public class FoodController {
 
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     @PostMapping
-    public void saveFood(@RequestBody FoodRequestDTO data){
-        Food foodData = new Food(data);
+    public void saveFood(@RequestBody FoodRequestDTO data) {
+        Food foodData = new Food( data);
         repository.save(foodData);
 
         return;
 
     }
+
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping()
-    public List<FoodResponseDTO> getAll(){
+    public List<FoodResponseDTO> getAll() {
 
         List<FoodResponseDTO> foodList = repository.findAll().stream().map(FoodResponseDTO::new).toList();
 
         return foodList;
+    }
+
+    @GetMapping("/food/{id}")
+    public List<FoodResponseDTO> getById(@PathVariable Long id) {
+
+        List<Food> foodList = repository.findAll();
+
+        List<FoodResponseDTO> filteredList = foodList
+                .stream()
+                .filter(food -> food.getId().equals(id))
+                .map(FoodResponseDTO::new)
+                .collect(Collectors.toList());
+
+
+
+        return filteredList;
     }
 
 
